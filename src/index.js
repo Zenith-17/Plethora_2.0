164c10ed87d4e964d29cb0f07106406142b7bd44
Atharva Bhatnagar <99113156+Zenith-17@users.noreply.github.com>
2023-11-27 18:12:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { store } from './store/store.js'
 import { Provider } from 'react-redux'
 
+//note provider is provided by react-redux as it provides an interface between the react and redux
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
