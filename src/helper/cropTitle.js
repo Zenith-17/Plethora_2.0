5c3bb311d29bab61bf7c86f7713d3519b890dc03
Atharva Bhatnagar <99113156+Zenith-17@users.noreply.github.com>
2023-11-27 18:09:21 +0530

    Update cropTitle.js

--- src/helper/cropTitle.js
+++ src/helper/cropTitle.js
@@ -1,3 +1,4 @@
+//this is will trim down the title if length of the title is greater than the given offset in the constants file
 function cropTitle(title, maxLength) {
     if (title.length <= maxLength) {
       return title;
