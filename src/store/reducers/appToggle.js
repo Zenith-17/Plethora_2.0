9dddb989eaf4304d8e6c185a67318ff847c1ea05
Atharva Bhatnagar <99113156+Zenith-17@users.noreply.github.com>
2023-11-27 18:10:52 +0530

    Update appToggle.js

--- src/store/reducers/appToggle.js
+++ src/store/reducers/appToggle.js
@@ -1,13 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isMenuOpen: true,
+  isMenuOpen: false,
 };
 
-export const appToggle = createSlice({
+ const appToggle = createSlice({
   name: "toggle_app",
   initialState,
+  //writing reducer functions corresponding to each action
+  //when we click on a component(like button) then it dispatches an action which will call these reducer functions
+  //reducer functions modify the state based on corresponding actions and that is why we export actions and reducer functions   
+  //payload consists of data passed 
   reducers: {
+    //means when toggle is called call the corresponding function defined to toggle and that is why we export actions as well as reducers both 
     toggle: (state) => {
       state.isMenuOpen = !state.isMenuOpen;
     },
@@ -17,6 +22,10 @@ export const appToggle = createSlice({
   },
 });
 
+//we export two things ie actions and reducers
+
+//exporting actions
 export const { toggle, closetoggle } = appToggle.actions;
 
+//exporting reducers
 export default appToggle.reducer;
