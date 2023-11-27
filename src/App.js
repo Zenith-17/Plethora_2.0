4a522d939b5f885e50b3d60e8dfc8a388178a5ab
Atharva Bhatnagar <99113156+Zenith-17@users.noreply.github.com>
2023-11-27 18:12:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import ErrorPage from "./components/ErrorPage";
 import VideoContainer from "./components/VideoContainer";
 import { lazy, Suspense} from "react";
 
+//lodaing other components inside home component in a lazy manner
+// using lazy and Suspense from React to enable lazy loading for certain components. 
+// Lazy loading means that these components will be loaded asynchronously (on-demand) when they are accessed, rather than loading them all at once when the application starts. This is beneficial for reducing the initial loading time and only fetching components as needed.
+
 const WatchPage = lazy(() => import("./components/WatchPage"));
 const SearchPage = lazy(() => import("./components/SearchPage"));
 const FilteredResults = lazy(() => import("./components/FilteredResults"));
